2680a2345648fe78bc6fa981888267ddfb6a1d18
lib26 <dladlsqja@naver.com>
2023-01-16 19:58:01 +0900

    static contents

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,14 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // 나는 컨트롤러이다
 public class HelloController {
 
-    @GetMapping("hello") // GET : http://localhost:8080/hello
+    @GetMapping("hello") // GET : http://localhost:8080/hello 를 치면 아래의 method를 호출해준다. 스프링에서 다 해줌
     public String hello(Model model){ // 스프링이 model을 받아와서
         model.addAttribute("data","hello!!"); // key와 value를 설정?해주고
         return "hello"; // hello.html로 가서 렌더링을 해라.
         // return "hello"는 기본적으로 스프링부트가 resources 안에 templates 밑에 hello.html을 찾도록 설계되어있다.
+        // 즉, 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버( viewResolver )가 화면을 찾아서 처리한다.
         // Thymeleaf 템플릿 엔진이 처리
     }
 }
