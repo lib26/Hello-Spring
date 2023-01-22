e3778d96e84ff20a61a75f0fcd831184c51c2285
lib26 <dladlsqja@naver.com>
2023-01-22 20:32:13 +0900

    jdbc template

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -3,7 +3,6 @@ import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
-import java.util.Optional;
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -10,6 +10,7 @@ import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
+
     MemberService memberService;
     MemoryMemberRepository memberRepository;
 
